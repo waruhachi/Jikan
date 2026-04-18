b8d52edc491f30b90587eebcd50d18c9ec1faa81
waru <156133757+waruhachi@users.noreply.github.com>
2026-04-18 15:33:29 -0500

    feat: move to respringless

--- Preferences/Controllers/JikanRootListController.h
+++ Preferences/Controllers/JikanRootListController.h
@@ -1,8 +1,6 @@
 #include <Preferences/PSListController.h>
 #include <Preferences/PSSpecifier.h>
 #include <UIKit/UIKit.h>
-#include <roothide.h>
-#include <spawn.h>
 
 @interface JikanRootListController : PSListController
 @end
--- Preferences/Controllers/JikanRootListController.m
+++ Preferences/Controllers/JikanRootListController.m
@@ -14,13 +14,4 @@
 	[super setPreferenceValue:value specifier:specifier];
 }
 
-- (void)respring {
-	pid_t pid;
-	const char *args[] = {"killall", "SpringBoard", NULL};
-	int result = posix_spawn(&pid, jbroot("/usr/bin/killall"), NULL, NULL, (char *const *)args, NULL);
-	if (result != 0) {
-		NSLog(@"[JikanPreferences] Failed to respring SpringBoard: %d", result);
-	}
-}
-
 @end
